d787949cbef4522df9eeae11d8f29264e1e81bfa
BaiJianM <13691762041@163.com>
2025-01-23 16:49:52 +0800

    1

---
+++ server/src/main/java/com/liyuyouguo/server/mapper/KeywordsMapper.java
@@ -0,0 +1,13 @@
+package com.liyuyouguo.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liyuyouguo.server.entity.shop.Keywords;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author baijianmin
+ */
+@Repository
+public interface KeywordsMapper extends BaseMapper<Keywords> {
+
+}
